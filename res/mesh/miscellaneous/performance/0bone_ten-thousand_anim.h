1dfc27d246e9f0a91228de4421f72caa5ff45015
Jackson Null <actionjax4@gmail.com>
2021-12-24 12:53:18 -0800

    'C:\Users\ididi\LuClient'

---
+++ res/mesh/miscellaneous/performance/0bone_ten-thousand_anim.h
@@ -0,0 +1,16 @@
+// This file was automatically generated. It contains definitions for all the
+// animations stored in the associated KFM file. Include this file in your
+// final application to easily refer to animation sequences.
+
+#ifndef 0BONE_TEN-THOUSAND_ANIM_H__
+#define 0BONE_TEN-THOUSAND_ANIM_H__
+
+namespace 0bone_ten-thousand_Anim
+{
+    enum
+    {
+        TEST-OBJ_IDLE           = 0
+    };
+}
+
+#endif  // #ifndef 0BONE_TEN-THOUSAND_ANIM_H__
---
+++ res/mesh/miscellaneous/performance/5bone_ten-thousand_anim.h
@@ -0,0 +1,16 @@
+// This file was automatically generated. It contains definitions for all the
+// animations stored in the associated KFM file. Include this file in your
+// final application to easily refer to animation sequences.
+
+#ifndef 5BONE_TEN-THOUSAND_ANIM_H__
+#define 5BONE_TEN-THOUSAND_ANIM_H__
+
+namespace 5bone_ten-thousand_Anim
+{
+    enum
+    {
+        TEST-OBJ_IDLE           = 0
+    };
+}
+
+#endif  // #ifndef 5BONE_TEN-THOUSAND_ANIM_H__
